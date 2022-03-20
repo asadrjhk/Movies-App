2ddfca13f8e15d02eefe540118abbc6b8adae989
Asad Alam <asad.alam4589@gmail.com>
2022-03-20 16:32:08 +0530

    Added pagination component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,15 +1,48 @@
-import React, { useState } from 'react'
-import { movies } from './getMovies'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {Spinner, Card, Button, Pagination} from 'react-bootstrap';
+
+const API_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=';
 
 const Movies = () => {
     const [hover, setHover] = useState('');
     const [pageNumber, setPageNumber] = useState([1]);
-    const movie = movies.results;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [movies, setMovies] = useState([]);
+    
+
+    const hook = () => {
+        axios.get(`${API_URL}${currentPage}`)
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                setMovies([...data.results])
+            })
+    }
+    useEffect(hook, [currentPage]);
+
+    const handleNext = () => {
+        setCurrentPage(pageNumber[pageNumber.length - 1] + 1);
+        setPageNumber(pageNumber.concat(pageNumber[pageNumber.length - 1] + 1));
+         
+    }
+    const handlePrev = (page) => {
+        if (page !== 1) {
+            setCurrentPage((prevPage) => prevPage - 1);
+            // pageNumber.pop()
+            setPageNumber(pageNumber.slice(0, -1));
+
+        }
+    }
+    const handleClickPage = (page) => {
+        if (page !== currentPage) {
+            setCurrentPage(page);
+        }
+        console.log("current-page: ", currentPage);
+    }
     return (
          <>
-            { movie.length === 0 ? (
+            { movies.length === 0 ? (
                 <Spinner animation="border" role="status" variant="primary">
                     <span className="visually-hidden">Loading...</span>
                 </Spinner>
@@ -18,7 +51,7 @@ const Movies = () => {
                    <h3 className="text-center">Trending Movies</h3>
                    <div className="d-flex flex-wrap gap-1">
                        {
-                           movie.map((movieObj) => {
+                           movies.map((movieObj) => {
                                return (
                                 <Card  key={movieObj.id} style={{ width: '18rem' }} onMouseEnter={() => setHover(movieObj.id)} onMouseLeave={() => setHover('')}>
                                     <Card.Header>Featured</Card.Header>
@@ -39,15 +72,15 @@ const Movies = () => {
                </div>
             )}
                 <Pagination className="justify-content-center p-3">
-                    <Pagination.Prev />
+                    <Pagination.Prev onClick={() => handlePrev(currentPage)} />
                      
                      {  pageNumber.map((value) => (
-                            <Pagination.Item>{value}</Pagination.Item>
+                            <Pagination.Item onClick={() => handleClickPage(value)}>{value}</Pagination.Item>
                         ))
                           
                      }
                      
-                    <Pagination.Next />
+                    <Pagination.Next onClick={handleNext}/>
                 </Pagination>
          </>
     )
